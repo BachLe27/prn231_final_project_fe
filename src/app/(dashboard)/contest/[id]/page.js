d4cc4a6ba9556bf0eb358b32327f3daf90728c95
BachLe27 <bachlamsung123@gmail.com>
2024-03-23 11:40:27 +0700

    update chatgpt prompt

--- src/app/(dashboard)/contest/[id]/page.js
+++ src/app/(dashboard)/contest/[id]/page.js
@@ -1,84 +1,95 @@
 'use client'
-import useAddSubmission from '@/data/useAddSubmission';
-import useContestById from '@/data/useContestById';
-import useGetSubmission from '@/data/useGetSubmission';
-import useSubject from '@/data/useSubject';
-import useSubjectById from '@/data/useSubjectById';
-import useSubmissionById from '@/data/useSubmissionById';
-import useSubmissionsByContest from '@/data/useSubmissionsByContest';
-import useAuth from '@/hook/useAuth';
-import { ExclamationCircleOutlined, UserOutlined } from '@ant-design/icons';
-import { Button, Col, Divider, Flex, Form, Input, Modal, Row, Typography, message } from 'antd';
-import moment from 'moment';
-import { usePathname } from 'next/navigation';
+import useAddSubmission from '@/data/useAddSubmission'
+import useContestById from '@/data/useContestById'
+import useGetSubmission from '@/data/useGetSubmission'
+import useSubject from '@/data/useSubject'
+import useSubjectById from '@/data/useSubjectById'
+import useSubmissionById from '@/data/useSubmissionById'
+import useSubmissionsByContest from '@/data/useSubmissionsByContest'
+import useAuth from '@/hook/useAuth'
+import { ExclamationCircleOutlined, UserOutlined } from '@ant-design/icons'
+import { Button, Col, Divider, Flex, Form, Input, Modal, Row, Select, Typography, message } from 'antd'
+import moment from 'moment'
+import { usePathname } from 'next/navigation'
 
 import React, { useEffect, useState } from 'react'
+import { CopyBlock } from 'react-code-blocks'
 
 const page = () => {
+  const pathname = usePathname()
+  const segments = pathname.split('/')
+  const id = segments[segments.length - 1]
 
-  const pathname = usePathname();
-  const segments = pathname.split('/');
-  const id = segments[segments.length - 1];
-
-  const { data } = useContestById(id);
-  const { data: subject } = useSubjectById(data?.subjectId);
-  const [remainingTime, setRemainingTime] = useState(getRemainingTime());
-  const [loading, setLoading] = useState(false);
-  const { me } = useAuth();
+  const { data } = useContestById(id)
+  const { data: subject } = useSubjectById(data?.subjectId)
+  const [remainingTime, setRemainingTime] = useState(getRemainingTime())
+  const [loading, setLoading] = useState(false)
+  const { me } = useAuth()
   const { data: haveSubmission, refetch } = useGetSubmission({
     contestId: id,
     studentId: me.id
   })
 
-  const [modal, contextHolder] = Modal.useModal();
+  const [content, setContent] = useState(haveSubmission?.content)
+  useEffect(() => {
+    setContent(haveSubmission?.content)
+  }, [content, haveSubmission])
+
+  const [modal, contextHolder] = Modal.useModal()
 
   const confirm = async () => {
     return await modal.confirm({
       title: 'Confirm',
       icon: <ExclamationCircleOutlined />,
       content: 'Bạn chỉ có thể nộp bài 1 lần duy nhất và không thể sửa bài làm',
-      centered: true,
-    });
-  };
-
+      centered: true
+    })
+  }
 
   function getRemainingTime() {
-    const endTime = moment(data?.endTime);
-    const currentTime = moment();
-    const diff = endTime.diff(currentTime);
-    const duration = moment.duration(diff);
+    const endTime = moment(data?.endTime)
+    const currentTime = moment()
+    const diff = endTime.diff(currentTime)
+    const duration = moment.duration(diff)
 
     return {
       days: duration.days(),
       hours: duration.hours(),
       minutes: duration.minutes(),
       seconds: duration.seconds()
-    };
+    }
   }
 
   const { submitSolutionMutation } = useAddSubmission()
 
   const [form] = Form.useForm()
   const onFinish = async (values) => {
-    const ok = await confirm();
-    if (!ok) return;
+    const ok = await confirm()
+    if (!ok) return
 
-    setLoading(true);
+    setLoading(true)
     submitSolutionMutation.mutate({
       ...values,
       studentId: me.id,
       contestId: id,
-      submissionTime: new Date().toISOString(),
+      submissionTime: new Date().toISOString()
     }, {
       onSuccess: () => {
-        message.success("Nộp bài thành công")
+        message.success('Nộp bài thành công')
         refetch()
       },
       onError: (err) => {
-        message.error("Nộp bài thất bại, " + err.response.data)
+        message.error('Nộp bài thất bại, ' + err.response.data)
       }
     })
-    setLoading(false);
+    setLoading(false)
+  }
+
+  const [language, setLanguage] = useState('C')
+
+  const handleChange = (value) => {
+    setLanguage(value)
+    console.log(language)
   }
 
   useEffect(() => {
@@ -104,11 +115,11 @@ const page = () => {
           </Typography.Title>
 
           <Typography.Title level={5} style={{ margin: 0 }}>
-            Từ {moment(data?.startTime).format("HH:mm:ss - DD MMMM, YYYY")} {' '}
+            Từ {moment(data?.startTime).format('HH:mm:ss - DD MMMM, YYYY')} {' '}
           </Typography.Title>
 
           <Typography.Title level={5} style={{ margin: 0 }}>
-            Đến {moment(data?.endTime).format("HH:mm:ss - DD MMMM, YYYY")}
+            Đến {moment(data?.endTime).format('HH:mm:ss - DD MMMM, YYYY')}
           </Typography.Title>
 
           <Typography.Title level={5} style={{ margin: 0 }}>
@@ -145,26 +156,52 @@ const page = () => {
         wrapperCol={{ span: 16 }}
       >
         <Form.Item
+          name="language"
+          label="Bài làm"
+          rules={[{ required: true, message: 'Vui lòng làm bài!' }]}
+        >
+          <Select
+            defaultValue="C"
+            style={{ width: 120 }}
+            onChange={handleChange}
+            options={[
+              { value: 'C', label: 'C' },
+              { value: 'java', label: 'Java' },
+              { value: 'javascript', label: 'JavaScript' },
+              { value: 'python', label: 'Python' }
+            ]}
+          />
+        </Form.Item>
+
+        <Form.Item
           name="content"
           label="Bài làm"
           rules={[{ required: true, message: 'Vui lòng làm bài!' }]}
         >
-          <Input.TextArea readOnly={haveSubmission} style={{ height: 300 }} placeholder="Điền nội dung bài làm vào đây" />
+          <Input.TextArea style={{ height: 300 }} placeholder="Điền nội dung bài làm vào đây"
+            onChange={(e) => {
+              setContent(e.target.value)
+            }} />
         </Form.Item>
 
-        {
-          !haveSubmission ?
-            <Form.Item wrapperCol={{ offset: 8, span: 8 }}>
-              <Button type="primary" htmlType="submit" loading={loading} style={{ width: '100%' }}>
-                Nộp bài
-              </Button>
-            </Form.Item> :
-            <Form.Item wrapperCol={{ offset: 8, span: 8 }}>
-              <Button type="primary" htmlType="submit" loading={loading} style={{ width: '100%' }} disabled>
-                Bạn đã nộp bài
-              </Button>
-            </Form.Item>
-        }
+        <Flex vertical>
+          <Typography.Title level={4}>Preview</Typography.Title>
+          <Flex>
+
+            <CopyBlock
+              text={content}
+              language={language}
+              showLineNumbers={true}
+              wrapLines
+            />
+          </Flex>
+        </Flex>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 8 }}>
+          <Button type="primary" htmlType="submit" loading={loading} style={{ width: '100%' }}>
+            Nộp bài
+          </Button>
+        </Form.Item>
         {contextHolder}
       </Form>
     </Flex >
--- src/app/(dashboard)/grade/[id]/page.js
+++ src/app/(dashboard)/grade/[id]/page.js
@@ -1,59 +1,105 @@
 'use client'
-import React, { useEffect, useState } from 'react';
-import { Button, Col, Divider, Flex, Row, Typography, message } from 'antd';
-import moment from 'moment';
-import { usePathname } from 'next/navigation';
-import useSubmissionById from '@/data/useSubmissionById';
-import 'moment/locale/vi';
-import { ClockCircleOutlined, CommentOutlined, MessageOutlined, OpenAIOutlined, SignatureOutlined, SmileOutlined, UserOutlined } from '@ant-design/icons';
-import AddGradeModal from './(components)/AddGradeModal';
-import useChatGPT from '@/data/useChatGPT';
+import useChatGPT from '@/data/useChatGPT'
+import useGrade from '@/data/useGrade'
+import useSubmissionById from '@/data/useSubmissionById'
+import { ClockCircleOutlined, CommentOutlined, HighlightOutlined, MessageOutlined, OpenAIOutlined, SignatureOutlined, SmileOutlined, UserOutlined } from '@ant-design/icons'
+import { Button, Col, Divider, Flex, Form, Input, Row, Select, Typography, message } from 'antd'
+import moment from 'moment'
+import 'moment/locale/vi'
+import { usePathname } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import { CopyBlock } from 'react-code-blocks'
+import AddGradeModal from './(components)/AddGradeModal'
 
-moment.locale('vi');
+moment.locale('vi')
 
 const Grade = () => {
-  const pathname = usePathname();
-  const segments = pathname.split('/');
-  const id = segments[segments.length - 1];
+  const pathname = usePathname()
+  const segments = pathname.split('/')
+  const id = segments[segments.length - 1]
 
-  const { data, refetch } = useSubmissionById(id);
+  const { data, refetch } = useSubmissionById(id)
 
-  const [chatGPTText, setChatGPTText] = useState();
+  const [form] = Form.useForm()
+  const [loading, setLoading] = useState(false)
+  const { updateSubmissionsMutation } = useGrade()
+
+  useEffect(() => {
+    form.setFieldsValue({ ...data })
+  }, [form, data])
+
+  const onFinish = (values) => {
+    setLoading(true)
+    updateSubmissionsMutation.mutate({
+      studentId: data.student.id,
+      contestId: data.contest.id,
+      ...data,
+      grade: Number(values.grade),
+      ...values
+    },
+      {
+        onSuccess: async (data) => {
+          message.success('Chấm bài thành công')
+          form.setFieldsValue({ ...data.data })
+          refetch()
+          handleCancel()
+        },
+        onError: () => {
+          message.error('Chấm bài thất bại')
+        }
+      })
+    setLoading(false)
+  }
+
+  const [chatGPTText, setChatGPTText] = useState()
 
   const { getChatGPTMutation } = useChatGPT()
 
   const displayTextCharacterByCharacter = (text) => {
-    let currentIndex = 0;
+    let currentIndex = 0
     const interval = setInterval(() => {
       if (currentIndex <= text.length) {
-        setChatGPTText(text.substring(0, currentIndex));
-        currentIndex++;
+        setChatGPTText(text.substring(0, currentIndex))
+        currentIndex++
       } else {
-        clearInterval(interval);
+        clearInterval(interval)
       }
-    }, 30); // Adjust the interval duration (in milliseconds) for the desired typing speed
-  };
+    }, 10) // Adjust the interval duration (in milliseconds) for the desired typing speed
+  }
 
   const handleOpenChatModal = () => {
     getChatGPTMutation.mutate(
       {
         requirement: `
-        Hãy chấm bài sau theo thang điểm 10 một cách khó tính nhất và giải thích tại sao lại cho điểm như thế. 
+        Hãy chấm bài sau theo thang điểm 10 và trả về nhận xét theo những phần nội dung như sau: 
+        - Phân tích chi tiết:
+          + Ưu điểm: ...
+          + Nhược điểm: ...
+          + Các lỗi cú pháp: ...
+          + Các lỗi logic có thể xảy ra khi chạy chương trình: ...
+          - Lý do trừ điểm: ...
+          + Mức độ đáp ứng yêu cầu: ...
+        - Số điểm(theo thang điểm 10): ...
       `,
         questions: data?.contest.question,
         student_answer: data?.content
       }, {
       onSuccess: (data) => {
-        const chatGPTContent = data.data.content;
-        displayTextCharacterByCharacter(chatGPTContent);
+        const chatGPTContent = data.data.textContent
+        displayTextCharacterByCharacter(chatGPTContent)
       },
       onError: () => {
-        setChatGPTText("Đã có lỗi xảy ra, vui lòng thử lại sau");
+        setChatGPTText('Đã có lỗi xảy ra, vui lòng thử lại sau')
       }
     }
     )
-  };
+  }
 
+  const [language, setLanguage] = useState('C')
+
+  const handleChange = (value) => {
+    setLanguage(value)
+  }
 
   const [isModalOpen, setIsModalOpen] = useState(false)
 
@@ -72,7 +118,7 @@ const Grade = () => {
   return (
     <Flex vertical gap={32}>
       <Flex gap={32}>
-        <Flex vertical gap={16} style={{ width: 500 }}>
+        <Flex vertical gap={16} style={{ width: 750 }}>
           <Typography.Title>Thông tin bài nộp</Typography.Title>
           <Typography.Title level={5} style={{ margin: 0 }}>
             Bài thi:{' '}
@@ -89,11 +135,11 @@ const Grade = () => {
 
           <Typography.Title level={5} style={{ margin: 0 }}>
 
-            Từ {moment(data?.contest?.startTime).format("HH:mm:ss - DD MMMM, YYYY")} {' '}
+            Từ {moment(data?.contest?.startTime).format('HH:mm:ss - DD MMMM, YYYY')} {' '}
           </Typography.Title>
 
           <Typography.Title level={5} style={{ margin: 0 }}>
-            Đến {moment(data?.contest?.endTime).format("HH:mm:ss - DD MMMM, YYYY")}
+            Đến {moment(data?.contest?.endTime).format('HH:mm:ss - DD MMMM, YYYY')}
           </Typography.Title>
 
           <Typography.Title level={5} style={{ margin: 0 }}>
@@ -103,47 +149,73 @@ const Grade = () => {
             </Typography.Text>
           </Typography.Title>
 
-
-
           <Typography.Title level={5} style={{ margin: 0 }}>
             <ClockCircleOutlined />{' '}Thời gian nộp:{' '}
             <Typography.Text level={5} style={{ color: 'blue', fontSize: 16 }}>
-              {moment(data?.submissionTime).format("HH:mm:ss - DD MMMM, YYYY")}
+              {moment(data?.submissionTime).format('HH:mm:ss - DD MMMM, YYYY')}
             </Typography.Text>
           </Typography.Title>
 
-          <Typography.Title level={5} style={{ margin: 0 }}>
-            <SignatureOutlined /> Điểm:{' '}
-            <Typography.Text level={5} style={{ color: 'blue', fontSize: 16 }}>
-              {data?.grade ? `${data.grade} / 10` : 'Chưa chấm'}
-            </Typography.Text>
-          </Typography.Title>
-          <Typography.Title level={5} style={{ margin: 0 }}>
-            <CommentOutlined /> Nhận xét:{' '}
-            <Typography.Text level={5} style={{ color: 'blue', fontSize: 16 }}>
-              {data?.teacherFeedback ?? 'Chưa có nhận xét'}
-            </Typography.Text>
-          </Typography.Title>
-          <Button size='large' onClick={showModal} icon={<SmileOutlined />}>
-            Chấm bài
-          </Button>
+          <Form
+            form={form}
+            style={{ width: '100%' }}
+            name="login"
+            onFinish={onFinish}
+            scrollToFirstError
+            size="large"
+            labelCol={{ span: 6 }}
+            labelAlign='left'
+            wrapperCol={{ span: 16 }}
+            initialValues={{
+              ...data
+            }}
+          >
+            <Form.Item
+              name="grade"
+              label="Điểm"
+              rules={[{ required: true, message: 'Vui lòng nhập Điểm!' }]}
+            >
+              <Input type='number' placeholder="Điểm" />
+            </Form.Item>
+
+            <Form.Item
+              name="teacherFeedback"
+              label="Nhận xét"
+            >
+              <Input.TextArea placeholder="Nhận xét" style={{ height: 250 }} />
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+              <Button type="primary" htmlType="submit" loading={loading} style={{ width: '100%' }}>
+                Cập nhật
+              </Button>
+            </Form.Item>
+          </Form>
+
         </Flex>
         <Divider type='vertical' style={{ minHeight: '400px' }} />
         <Flex vertical gap={30}>
           <Flex vertical>
             <Typography.Title><OpenAIOutlined /> ChatGPT Support</Typography.Title>
-            <Flex>
+            <Flex gap={16}>
               <Button size='large' onClick={handleOpenChatModal} loading={getChatGPTMutation.isPending} icon={<MessageOutlined />}>
                 Nhận xét của Chat GPT
               </Button>
+              {
+                chatGPTText && <Button size='large' icon={<HighlightOutlined />} onClick={
+                  () => {
+                    form.setFieldValue('teacherFeedback', chatGPTText)
+                  }
+                } >Lấy nhận xét</Button>
+              }
+
             </Flex>
           </Flex>
           <Flex style={{ width: 450 }}>
-            <Typography.Title level={4}>{chatGPTText}</Typography.Title>
+            <Typography.Title level={4} style={{ whiteSpace: 'pre-line' }}>{chatGPTText}</Typography.Title>
           </Flex>
         </Flex>
       </Flex>
-
 
       <Divider type='vertical' />
       <Row>
@@ -162,9 +234,28 @@ const Grade = () => {
           <Typography.Title level={3}>
             Bài làm của thí sinh:
           </Typography.Title>
-          <Typography.Text>
-            {data?.content}
-          </Typography.Text>
+
+          <Flex align='center' gap={4} style={{ marginBottom: 16 }}>
+            <Typography>Chọn ngôn ngữ: </Typography>
+            <Select
+              defaultValue="C"
+              style={{ width: 120 }}
+              onChange={handleChange}
+              options={[
+                { value: 'C', label: 'C' },
+                { value: 'java', label: 'Java' },
+                { value: 'javascript', label: 'JavaScript' },
+                { value: 'python', label: 'Python' }
+              ]}
+            />
+          </Flex>
+
+          <CopyBlock
+            text={data?.content}
+            language={language}
+            showLineNumbers={true}
+            wrapLines
+          />
         </Col>
       </Row>
 
@@ -176,7 +267,7 @@ const Grade = () => {
         refetch={refetch}
       />
     </Flex>
-  );
-};
+  )
+}
 
-export default Grade;
+export default Grade
